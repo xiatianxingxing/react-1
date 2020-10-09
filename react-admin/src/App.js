9208a973bc29bafae80f8d1c514c16eab102d8dd
dongpeng <dongpeng@yonyou.com>
2020-10-09 11:30:58 +0800

    star

---
+++ react-admin/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       我是react项目
+    </div>
+  );
+}
+
+export default App;
